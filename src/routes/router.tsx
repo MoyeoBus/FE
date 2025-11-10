0d1baf37ca2de3cf9be4f84dc033d471edc1785c
SonYoungJun <dudwnsths1@naver.com>
2025-11-11 00:12:13 +0900

    feat: 메인화면 구성을 위한 라우팅 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,6 +4,9 @@ import Splash from "../pages/Splash";
 import Login from "../pages/Login";
 import AccountLogin from "../pages/AccountLogin";
 import Home from "../pages/Home";
+import RouteHistory from "../pages/RouteHistory";
+import RouteDetail from "../pages/RouteDetail";
+import MyPage from "../pages/MyPage";
 
 export const router = createBrowserRouter([
   {
@@ -11,9 +14,12 @@ export const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { index: true, element: <Splash /> },
-      { path: "login", element: <Login /> },
-      { path: "login/account", element: <AccountLogin /> },
       { path: "home", element: <Home /> },
+      { path: "history", element: <RouteHistory /> },
+      { path: "history/:id", element: <RouteDetail /> },
+      { path: "mypage", element: <MyPage /> },
     ],
   },
+  { path: "login", element: <Login /> },
+  { path: "login/account", element: <AccountLogin /> },
 ]);

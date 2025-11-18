a3a026dc413b7b32afb0f7890d35086b60c80b5f
SonYoungJun <dudwnsths1@naver.com>
2025-11-19 03:46:16 +0900

    feat: api 주소 변경

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://133.186.143.54:8080/api/v1",
+  baseURL: "https://moyeobus.com/api/v1",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",

769fdfcf60f25df08f1f0483555ded839a84cbfd
SonYoungJun <dudwnsths1@naver.com>
2025-12-29 01:52:33 +0900

    fix: 400 에러 수정

--- src/apis/authApi.ts
+++ src/apis/authApi.ts
@@ -14,6 +14,6 @@ export async function login(payload: LoginPayload) {
 
 export async function loginOAuth(provider: Provider) {
   return axiosInstance.post("/login/oauth", null, {
-    params: { provider },
+    params: { provider: provider },
   });
 }
